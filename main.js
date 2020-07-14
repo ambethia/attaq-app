5bbe9d5c3bd0fb8930963ed51d134d5e93f6aedf
Jason L Perry <jason@suncoast.io>
2020-07-14 13:21:43 -0400

    Update
    A lazy commit.

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const { app, BrowserWindow, globalShortcut } = require("electron");
+const { app, BrowserWindow } = require("electron");
 const windowStateKeeper = require("electron-window-state");
 const robot = require("robotjs");
 const jsQR = require("jsqr");
-const path = require("path");
+const ioHook = require("iohook");
 
-let currentKeyCode;
-let mainWindow;
+let currentKeyCode, mainWindow, throttleId;
 let x, y, width, height;
+
+const TIMING = 500;
 
 function setup() {
   let mainWindowState = windowStateKeeper({
     defaultWidth: 100,
     defaultHeight: 100,
   });
-  const win = new BrowserWindow({
+
+  mainWindow = new BrowserWindow({
     frame: false,
     transparent: true,
     x: mainWindowState.x,
@@ -25,18 +27,18 @@ function setup() {
       nodeIntegration: true,
     },
   });
-  mainWindowState.manage(win);
-  win.loadFile("index.html");
-  mainWindow = win;
 
-  win.on("resize", setGeometry);
-  win.on("move", setGeometry);
-  setGeometry();
-
-  const ret = globalShortcut.register("`", pressGlobalKey);
-  if (!ret) console.warn("Registration of shortcut failed.");
+  mainWindowState.manage(mainWindow);
+  mainWindow.loadFile("index.html");
+  mainWindow.on("resize", setGeometry);
+  mainWindow.on("move", setGeometry);
   mainWindow.webContents.send("change-state", "inactive");
-  setInterval(getKeyCode, 1000);
+
+  ioHook.on("mousewheel", handleGlobalTrigger);
+  ioHook.start();
+
+  setGeometry();
+  setInterval(getKeyCode, TIMING);
 }
 
 function getKeyCode() {
@@ -66,15 +68,18 @@ function getKeyCode() {
   } catch (error) {}
 }
 
-let timerId;
-function pressGlobalKey() {
-	if (timerId) return
-	timerId = setTimeout(()  => {
+function handleGlobalTrigger() {
+  if (throttleId) return;
+  throttleId = setTimeout(() => {
     if (currentKeyCode) {
-      robot.keyTap(currentKeyCode);
+      try {
+        robot.keyTap(currentKeyCode);
+      } catch (error) {
+        console.warn(`Invalid Key: ${currentKeyCode}`);
+      }
     }
-		timerId = undefined;
-	}, 500)
+    throttleId = undefined;
+  }, TIMING);
 }
 
 function setGeometry() {
